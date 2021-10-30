6860d0c29429685142c3f4b0ba712079bcc4ed32
Sarah <sarahrudy@gmail.com>
2021-10-30 16:21:58 -0600

    Add api.listen to PORT

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const PORT = process.env.PORT || 3001
+api.listen(PORT, () => {
+  console.warn(`App listening on http://localhost:${PORT}`)
+})
 
 const express = require('express')
 const axios = require('axios')
