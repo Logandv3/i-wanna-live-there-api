ebacfbef60535df7b8d83268a86fa8d7c06720a4
Sarah <sarahrudy@gmail.com>
2021-10-30 12:58:15 -0600

    Install dependencies

---
+++ index.js
@@ -0,0 +1,72 @@
+const PORT = 3001
+const express = require('express')
+const axios = require('axios')
+const cheerio = require('cheerio')
+const { response } = require('express')
+const app = express()
+
+const newspapers = [
+  {
+    name: 'thetimes',
+    address: 'https://www.thetimes.co.uk/environment/climate-change',
+    base: '',
+  },
+  {
+    name: 'guardian',
+    address: 'https://www.theguardian.co.uk/environment/climate-crisis',
+    base: '',
+  },
+  {
+    name: 'telegraph',
+    address: 'https://www.telegraph.co.uk/climate-change',
+    base: 'https://www.telegraph.co.uk',
+  }
+]
+
+const articles = []
+
+newspapers.forEach(newspaper => {
+  axios.get(newspaper.address)
+      .then(response => {
+        const html = response.data
+        const $ = cheerio.load(html)
+
+        $('a:contains("climate")', html).each(function () {
+            const title = $(this).text()
+            const url = $(this).attr('href')
+
+            articles.push({
+              title,
+              url: newspaper.base + url,
+              source: newspaper.name
+            })
+        })
+
+      })
+})
+
+app.get('/', (req, res) => {
+  res.json('Welcome to our API')
+})
+
+app.get('/news', (req, res) => {
+  res.json(articles)
+})
+
+app.listen(PORT, () => console.log(`server running on ${PORT}`))
+
+// axios.get('https://www.theguardian.com/environment/climate-crisis')
+//     .then(response => {
+//       const html = response.data
+//       const $ = cheerio.load(html)
+
+//       $('a:contains("climate")', html).each(function () {
+//         const title = $(this).text()
+//         const url = $(this).attr('href')
+//         articles.push({
+//             title,
+//             url
+//         })
+//       })
+//       res.json(articles)
+//     }).catch((err) => console.log(err))
