bad25608c52774ce41bdc2c579ce12c9f577aa87
Sarah <sarahrudy@gmail.com>
2021-10-30 13:22:07 -0600

    Install nodemon dependency

--- index.js
+++ index.js
@@ -27,22 +27,22 @@ const articles = []
 
 newspapers.forEach(newspaper => {
   axios.get(newspaper.address)
-      .then(response => {
-        const html = response.data
-        const $ = cheerio.load(html)
+    .then(response => {
+      const html = response.data
+      const $ = cheerio.load(html)
 
-        $('a:contains("climate")', html).each(function () {
-            const title = $(this).text()
-            const url = $(this).attr('href')
+      $('a:contains("climate")', html).each(function () {
+        const title = $(this).text()
+        const url = $(this).attr('href')
 
-            articles.push({
-              title,
-              url: newspaper.base + url,
-              source: newspaper.name
-            })
+        articles.push({
+          title,
+          url: newspaper.base + url,
+          source: newspaper.name
         })
-
       })
+
+    })
 })
 
 app.get('/', (req, res) => {
@@ -51,6 +51,33 @@ app.get('/', (req, res) => {
 
 app.get('/news', (req, res) => {
   res.json(articles)
+})
+
+app.get('/news/:newspaperId', (req, res) => {
+  const newspaperId = req.params.newspaperId
+
+  const newspaperAddress = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].address
+  const newspaperBase = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].base
+
+  axios.get(newspaperAddress)
+    .then(response => {
+      const html = response.data
+      const $ = cheerio.load(html)
+      const specificArticles = []
+
+      $('a:contains("climate")', html).each(function () {
+        const title = $(this).text()
+        const url = $(this).attr('href')
+        specificArticles.push({
+          title,
+          url: newspaperBase + url,
+          source: newspaperId
+        })
+      })
+      res.json(specificArticles)
+    }).catch(err => console.log(err))
+
+
 })
 
 app.listen(PORT, () => console.log(`server running on ${PORT}`))
