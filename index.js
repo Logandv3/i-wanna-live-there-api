ab71540d5c149dfc3ed4031729b8f36f7e09d7e8
Sarah <sarahrudy@gmail.com>
2021-11-01 19:30:33 -0600

    Install pg

--- index.js
+++ index.js
@@ -80,21 +80,5 @@ app.get('/news/:newspaperId', (req, res) => {
 
 
 })
-// Hey there friends
+
 app.listen(PORT, () => console.log(`server running on ${PORT}`))
-
-// axios.get('https://www.theguardian.com/environment/climate-crisis')
-//     .then(response => {
-//       const html = response.data
-//       const $ = cheerio.load(html)
-
-//       $('a:contains("climate")', html).each(function () {
-//         const title = $(this).text()
-//         const url = $(this).attr('href')
-//         articles.push({
-//             title,
-//             url
-//         })
-//       })
-//       res.json(articles)
-//     }).catch((err) => console.log(err))
